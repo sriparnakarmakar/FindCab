8871bed4e0f64af36226b28d44d64dddfba27a2c
sriparnakarmakar <sriparna.cse@gmail.com>
2020-11-13 01:14:32 +0530

    commit

---
+++ FindCab/FindCab/Models/BookingResponse.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindCab.Models
+{
+    public class BookingResponse
+    {
+        public string data { get; set; }
+    }
+}
---
+++ FindCab/FindCab/Models/CommonDeleteModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FindCab.Models
+{
+    [NotMapped]
+    public class CommonDeleteModel
+    {
+        public string Username { get; set; }
+        public int id { get; set; }
+    }
+}
